083a7f6a02944a73b646b7a052eb17ba4b6cb01b
Kulyash Konyrova <konyrova@gmail.com>
2019-10-29 17:17:09 +0600

    serializers

---
+++ api/serializers.py
@@ -0,0 +1,26 @@
+from django.db import transaction
+from rest_framework import serializers
+from .models import MainUser, Company, Vacancy
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MainUser
+        fields = '__all__'
+
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = '__all__'
+
+
+class VacancySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vacancy
+        fields = '__all__'
+
+    def validate_salary(self, value):
+        if value > 0:
+            raise serializers.ValidationError('invalid salary')
+        return value
